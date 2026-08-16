ea3e021fdf461afd1db30aeeeb6da662371faba2
Fritzterfly <fritzfabila465@gmail.com>
2026-08-17 01:31:38 +0800

    Add question model and database structure

---
+++ app/Models/Question.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Question extends Model
+{
+    protected $fillable = [
+        'quiz_id',
+        'question_text',
+    ];
+
+    public function quiz(): BelongsTo
+    {
+        return $this->belongsTo(Quiz::class);
+    }
+}
--- app/Models/Quiz.php
+++ app/Models/Quiz.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Quiz extends Model
 {
@@ -24,5 +25,10 @@ class Quiz extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function questions(): HasMany
+    {
+        return $this->hasMany(Question::class);
     }
 }
