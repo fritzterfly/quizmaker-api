7a4c659c6a22b8367e3177d493274f34d31e7e86
Fritzterfly <fritzfabila465@gmail.com>
2026-08-17 01:05:11 +0800

    Add Sanctum logout endpoint

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 Route::get('/user', function (Request $request) {
     return $request->user();
